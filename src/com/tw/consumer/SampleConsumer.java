c32765a504e55975134886ce38c5af2142addb1e
Kurella Swathi <swathikurella4@gmail.com>
2020-10-13 17:39:43 +0530

    Add comments

--- src/com/tw/consumer/SampleConsumer.java
+++ src/com/tw/consumer/SampleConsumer.java
@@ -10,26 +10,26 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 public class SampleConsumer {
 
   private void consume() {
+    //Create Consumer Properties
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
     props.put("group.id", "test");
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    //Create Kafka Consumer
     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+    //Subscribe consumer to topics
     consumer.subscribe(Collections.singletonList("kafka-workshop-eg"));
-    boolean notExit = true;
-    do {
+    while (true) {
+      //Poll for records
       ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+      //Print records
       for (ConsumerRecord<String, String> record : records) {
-        boolean isNotExitMessage = !"exit".equalsIgnoreCase(record.value());
-        if(isNotExitMessage){
-          System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
-        }
-        notExit = notExit && isNotExitMessage;
-
+        System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
       }
+      //Commit the messages processed successfully
       consumer.commitSync();//TODO Remove this statement and see
-    } while(notExit);
+    }
   }
 
   public static void main(String[] args) {
--- src/com/tw/producer/SampleProducer.java
+++ src/com/tw/producer/SampleProducer.java
@@ -8,13 +8,16 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 public class SampleProducer {
 
   private void produce(String message) {
+    //Create Producer properties
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("acks", "1");
 
+    //Create Kafka Producer
     KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+    //Send data using Kafka Producer
     producer.send(new ProducerRecord<>("kafka-workshop-eg", message));
     //producer.flush();//TODO Read why need to flush before close
     producer.close();
